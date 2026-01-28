21e04f79b7684b192de47098fa94d7355914797a
Dimxnsity <ericflores689@gmail.com>
2026-01-27 18:45:57 -0600

    script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Estas segura?",
+    "Estas reaaalmente segura??",
+    "de verdad ESTAS segura?",
+    "porfi di que si:(",
+    "Solo piensalo poquiss",
+    "Andale :c",
+    "Porfi:(",
+    "andaleee mi vida:(",
+    "Me hare bolita",
+    "porfi, Te amoo! ❤️"
 ];
 
 let messageIndex = 0;
